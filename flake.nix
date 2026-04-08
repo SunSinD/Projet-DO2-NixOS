87119ac6b7dcbe2ecfb01fec553c11c1d63a5955
SunSinD <168051761+SunSinD@users.noreply.github.com>
2026-04-08 19:10:03 -0400

    Add initial flake.nix configuration

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "DO2 - Dons d'ordinateurs, 2e vie";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    disko.url   = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nixpkgs, ... }: {
+    nixosConfigurations.do2laptop = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs self; };
+      modules = [
+        inputs.disko.nixosModules.disko
+        ./disko-config.nix
+        ./configuration.nix
+      ];
+    };
+  };
+}
