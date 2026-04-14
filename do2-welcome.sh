93c807156364e4ebf88fbd39180ed744aab32b3a
SunSinD <168051761+SunSinD@users.noreply.github.com>
2026-04-14 01:17:52 -0400

    Update do2-welcome.sh

--- do2-welcome.sh
+++ do2-welcome.sh
@@ -6,7 +6,7 @@ touch "$MARKER"
 
 GDK_BACKEND=wayland yad \
   --title="DO2 Vérification" \
-  --text="<b>Installation terminée✓</b>\n\nAvant de remettre cet ordinateur, testez rapidement les applications installées et assurez-vous que tout fonctionne correctement.\n\nVérifiez aussi le Wi-Fi, le son, la webcam et le micro.\n\nSi tout est bon, passez au prochain PC!" \
+  --text="<b>Installation terminée</b>\n\nAvant de remettre cet ordinateur, testez rapidement les applications installées et assurez-vous que tout fonctionne correctement.\n\nVérifiez aussi le Wi-Fi, le son, la webcam et le micro.\n\nSi tout est bon, passez au prochain PC!" \
   --button="Compris:1" \
   --button="Site du projet:0" \
   --width=420 \
