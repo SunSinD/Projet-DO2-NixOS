8e22c69675b5bc5e113c7a486639c3a252ae49de
SunSinD <168051761+SunSinD@users.noreply.github.com>
2026-04-13 23:47:56 -0400

    Update do2-welcome.sh

--- do2-welcome.sh
+++ do2-welcome.sh
@@ -4,14 +4,9 @@ MARKER="$HOME/.do2-welcome-shown"
 [ -f "$MARKER" ] && exit 0
 touch "$MARKER"
 
-for i in $(seq 1 30); do
-  curl -s --max-time 1 https://sunsind.github.io > /dev/null 2>&1 && break
-  sleep 1
-done
-
-yad \
+GDK_BACKEND=wayland yad \
   --title="Bienvenue" \
-  --text="Cet ordinateur vous est remis dans le cadre du projet\n<b>Dons d'ordinateurs, 2e vie</b> du Collège Montmorency,\ninitié par des étudiants pour donner une seconde vie\nau matériel informatique.\n\nVisitez notre site pour en savoir plus." \
+  --text="Cet ordinateur vous est remis dans le cadre du projet <b>Dons d'ordinateurs, 2e vie</b> du Collège Montmorency, initié par des étudiants pour donner une seconde vie au matériel informatique.\n\nVisitez notre site pour en savoir plus." \
   --button="Fermer:1" \
   --button="Site Web:0" \
   --width=400 \
