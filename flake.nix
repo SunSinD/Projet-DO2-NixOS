ea43535882867a8196c3e7434fbbf89c32b0249a
SunSinD <168051761+SunSinD@users.noreply.github.com>
2026-04-15 00:46:55 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,27 +1,11 @@
 {
-  description = "DO2 - Dons d'ordinateurs, 2e vie — Stable & Compatible";
+  description = "DO2 - Dons d'ordinateurs, 2e vie — Projet Collège Montmorency";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    disko.url   = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
   };
-
-  outputs = inputs@{ self, nixpkgs, ... }: {
-    nixosConfigurations.do2 = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs self; };
-      modules = [
-        inputs.disko.nixosModules.disko
-        ./disko-config.nix
-        ./configuration.nix
-      ];
-    };
-  };
-}
 
   outputs = inputs@{ self, nixpkgs, ... }: {
     nixosConfigurations.do2 = nixpkgs.lib.nixosSystem {
@@ -32,7 +16,6 @@
       };
       modules = [
         inputs.disko.nixosModules.disko
-        inputs.nix-cachyos-kernel.nixosModules.default # Injects the performance kernel
         ./disko-config.nix
         ./configuration.nix
       ];
